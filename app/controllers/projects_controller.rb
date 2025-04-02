5ea85e92e673b8d10f1b9204a90a4f818feff8bf
Kartikey Chauhan <me@cskartikey.dev>
2025-04-03 00:48:32 +0530

    feat: implement explore projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,12 @@
 class ProjectsController < ApplicationController
+    before_action :authenticate_user!
     def index
-        @projects = Project.all
+        @projects = Project.includes(:user) 
+                          .order(rating: :desc)  
+    end
+
+    def my_projects
+        @projects = current_user.projects
+        render :index 
     end
 end
