a273d45e019a974bbccaa6e481b5c2a18a02d872
David M <62346025+dispherical@users.noreply.github.com>
2025-04-11 17:33:11 -0400

    fix api for /api/check_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   def check_user
     user = User.find_by(slack_id: params[:slack_id])
 
-    if user and user.projects.exists?
-      render json: { exists: true, has_project: true, project: user.projects }, status: :ok
+    if user and user.project
+      render json: { exists: true, has_project: true, project: user.project }, status: :ok
     elsif user
       render json: { exists: true, has_project: false }, status: :ok
     else
