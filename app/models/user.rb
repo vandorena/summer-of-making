ba009ac1c57f7b81801ef254eb8cd003a5e83ef7
Kartikey Chauhan <me@cskartikey.dev>
2025-04-12 21:00:59 +0530

    chore: run linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     has_many :followed_projects, through: :project_follows, source: :project
 
     validates :slack_id, presence: true, uniqueness: true
-    validates :email, :first_name,:last_name, :display_name, :timezone, :avatar, presence: true
+    validates :email, :first_name, :last_name, :display_name, :timezone, :avatar, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
     def self.find_or_create(auth)
@@ -14,14 +14,14 @@ class User < ApplicationRecord
         return user if user.persisted?
 
         client = Slack::Web::Client.new(token: ENV["SLACK_BOT_TOKEN"])
-        user_table = Airrecord.table(ENV['AIRTABLE_API_KEY'], ENV['AIRTABLE_BASE_ID'], "Users")
-        
+        user_table = Airrecord.table(ENV["AIRTABLE_API_KEY"], ENV["AIRTABLE_BASE_ID"], "Users")
+
         airtable_user = user_table.all(filter: "{Hack Club Slack ID} = '#{auth.info.authed_user.id}'").first
-        
+
         if airtable_user.nil?
           raise StandardError, "Please verify at https://forms.hackclub.com/eligibility"
         end
-        
+
         verification_status = airtable_user.fields["Verification Status"]
         if verification_status == "Eligible L1" || verification_status == "Eligible L2"
           puts "Found eligible user: #{airtable_user.id}: #{airtable_user.fields["First Name"]} #{airtable_user.fields["Last Name"]}"
@@ -31,7 +31,7 @@ class User < ApplicationRecord
         else
           raise StandardError, "You are not eligible. If you think this is an error, please DM @Bartoz on Slack."
         end
-        
+
         user_info = client.users_info(user: auth.info.authed_user.id)
         user.display_name = user_info.user.profile.display_name.presence || user_info.user.profile.real_name
         user.email = user_info.user.profile.email
