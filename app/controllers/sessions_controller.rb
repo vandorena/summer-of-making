d47827bff12ae63fb86b60d3091e8f61ddc506d9
Kartikey Chauhan <me@cskartikey.dev>
2025-04-13 18:38:35 +0530

    feat: implement basic logs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,30 @@
 class SessionsController < ApplicationController
     def create
       auth = request.env["omniauth.auth"]
+      
+      Rails.logger.tagged("Authentication") do
+        Rails.logger.info({
+          event: "auth_attempt",
+          slack_id: auth.info.authed_user.id,
+          ip: request.remote_ip,
+          user_agent: request.user_agent,
+          request_id: request.request_id
+        }.to_json)
+      end
 
       begin
         user = User.find_or_create(auth)
+        
+        Rails.logger.tagged("Authentication") do
+          Rails.logger.info({
+            event: "auth_success",
+            slack_id: auth.info.authed_user.id,
+            user_id: user.id,
+            email: user.email,
+            request_id: request.request_id
+          }.to_json)
+        end
+        
         session[:user_id] = user.id
         redirect_to my_projects_path, notice: "Signed in successfully!"
       rescue => e
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,32 @@ class User < ApplicationRecord
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
     def self.find_or_create(auth)
-        user = User.find_by(slack_id: auth.info.authed_user.id)
-        return user if user.present?
+        slack_id = auth.info.authed_user.id
+        
+        Rails.logger.tagged("UserCreation") do
+            Rails.logger.info({
+                event: "find_or_create_attempt",
+                slack_id: slack_id
+            }.to_json)
+        end
+
+        user = User.find_by(slack_id: slack_id)
+        if user.present?
+            Rails.logger.tagged("UserCreation") do
+                Rails.logger.info({
+                    event: "existing_user_found",
+                    slack_id: slack_id,
+                    user_id: user.id,
+                    email: user.email
+                }.to_json)
+            end
+            return user
+        end
 
         client = Slack::Web::Client.new(token: ENV["SLACK_BOT_TOKEN"])
         user_table = Airrecord.table(ENV["AIRTABLE_API_KEY"], ENV["AIRTABLE_BASE_ID"], "Users")
 
-        airtable_records = user_table.all(filter: "{Hack Club Slack ID} = '#{auth.info.authed_user.id}'")
+        airtable_records = user_table.all(filter: "{Hack Club Slack ID} = '#{slack_id}'")
         
         if airtable_records.empty?
           raise StandardError, "Please verify at https://forms.hackclub.com/eligibility"
@@ -29,10 +48,10 @@ class User < ApplicationRecord
           raise StandardError, "You are not eligible. If you think this is an error, please DM @Bartosz on Slack."
         end
 
-        user_info = client.users_info(user: auth.info.authed_user.id)
+        user_info = client.users_info(user: slack_id)
 
         user = User.new(
-          slack_id: auth.info.authed_user.id,
+          slack_id: slack_id,
           first_name: eligible_record.fields["First Name"],
           middle_name: eligible_record.fields["Middle Name"] || "",
           last_name: eligible_record.fields["Last Name"],
@@ -44,5 +63,5 @@ class User < ApplicationRecord
 
         user.save!
         user
-      end
+    end
 end
