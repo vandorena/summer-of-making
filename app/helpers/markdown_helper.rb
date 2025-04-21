69698faedcf1c77e12e0dd43abc635ffff2a6dbc
Kartikey Chauhan <me@cskartikey.dev>
2025-04-21 20:29:21 +0530

    feat: implement custom css for links, code blocks and code

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,17 +1,30 @@
 module MarkdownHelper
+  class CustomRender < Redcarpet::Render::HTML
+    def link(link, title, content)
+      "<a href='#{link}' title='#{title}' class='text-nice-blue underline'>#{content}</a>"
+    end
+
+    def block_code(code, language)
+      language ||= 'plaintext'
+      "<pre class='bg-gray-900 p-4 text-forest overflow-x-auto'><code class='language-#{language}'>#{code}</code></pre>"
+    end
+
+    def codespan(code)
+      "<code class='bg-gray-900 px-1 py-1 rounded-sm text-forest text-sm'>#{code}</code>"
+    end
+  end
+
   def markdown(text)
     return "" if text.blank?
 
     text = text.gsub(/\n\n/, "\n<br>\n")
-
-    renderer = Redcarpet::Render::HTML.new(
-      hard_wrap: true,
-      filter_html: true,
-      safe_links_only: true
-    )
     
     markdown = Redcarpet::Markdown.new(
-      renderer,
+      CustomRender.new(
+        hard_wrap: true,
+        filter_html: true,
+        safe_links_only: true
+      ),
       autolink: true,
       tables: true,
       fenced_code_blocks: true,
