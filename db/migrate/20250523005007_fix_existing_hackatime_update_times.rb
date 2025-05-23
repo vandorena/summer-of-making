f7480274ef3a1ed9a922e14c8eeb8237758b9a02
Kartikey Chauhan <me@cskartikey.dev>
2025-05-23 06:58:42 +0530

    fixes messed up time in retrospect

---
+++ db/migrate/20250523005007_fix_existing_hackatime_update_times.rb
@@ -0,0 +1,23 @@
+class FixExistingHackatimeUpdateTimes < ActiveRecord::Migration[8.0]
+  def up
+    Project.includes(:updates).where("hackatime_project_keys IS NOT NULL").find_each do |project|
+      updates_with_hackatime = project.updates
+                                      .where.not(last_hackatime_time: nil)
+                                      .order(:created_at)
+      
+      next if updates_with_hackatime.empty?
+      
+      previous_total = 0
+      
+      updates_with_hackatime.each do |update|
+        current_total = update.last_hackatime_time
+        actual_time_spent = current_total - previous_total
+        
+        # Update the record with the correct time spent for this specific update
+        update.update_column(:last_hackatime_time, actual_time_spent)
+        
+        previous_total = current_total
+      end
+    end
+  end
+end
