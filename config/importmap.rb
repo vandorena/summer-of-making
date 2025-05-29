1a13194365f10b8b3bd10777f4c07d768143f267
Kartikey Chauhan <me@cskartikey.dev>
2025-05-29 08:14:39 +0530

    remove editorjs

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,9 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "chartkick", to: "chartkick.js"
 pin "Chart.bundle", to: "Chart.bundle.js"
-pin "@editorjs/editorjs", to: "@editorjs--editorjs.js" # @2.30.8
-pin "@editorjs/code", to: "@editorjs--code.js" # @2.9.3
-pin "@editorjs/paragraph", to: "@editorjs--paragraph.js" # @2.11.7
 
 pin "@tiptap/core", to: "@tiptap--core.js" # @2.12.0
 pin "@tiptap/pm/commands", to: "@tiptap--pm--commands.js" # @2.12.0
