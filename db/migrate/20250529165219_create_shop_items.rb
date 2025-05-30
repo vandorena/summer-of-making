84c79eab04b608df87b273fa7fce00c95b5e91ad
Malted <malted@malted.dev>
2025-05-30 12:05:44 -0400

    Create ShopItem base class for STI

---
+++ db/migrate/20250529165219_create_shop_items.rb
@@ -0,0 +1,20 @@
+class CreateShopItems < ActiveRecord::Migration[8.0]
+  def change
+    create_table :shop_items do |t|
+      t.string :type
+      t.string :name
+      t.string :description
+      t.string :internal_description
+      t.decimal :actual_irl_fr_cost, precision: 6, scale: 2
+      t.decimal :cost, precision: 6, scale: 2
+      t.string :hacker_score{1.2}
+      t.boolean :requires_black_market
+      t.string :hcb_merchant_lock
+      t.string :hcb_category_lock
+      t.string :hcb_keyword_lock
+      t.jsonb :agh_contents
+
+      t.timestamps
+    end
+  end
+end
