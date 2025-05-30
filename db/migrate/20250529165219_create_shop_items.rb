46ed94bf2c8fd8345654d173bab262b462e6d865
Malted <malted@malted.dev>
2025-05-30 12:06:42 -0400

    Fix shopitem migration decimal precision

--- db/migrate/20250529165219_create_shop_items.rb
+++ db/migrate/20250529165219_create_shop_items.rb
@@ -7,7 +7,7 @@ class CreateShopItems < ActiveRecord::Migration[8.0]
       t.string :internal_description
       t.decimal :actual_irl_fr_cost, precision: 6, scale: 2
       t.decimal :cost, precision: 6, scale: 2
-      t.string :hacker_score{1.2}
+      t.string :hacker_score, precision: 1, scale: 2
       t.boolean :requires_black_market
       t.string :hcb_merchant_lock
       t.string :hcb_category_lock
