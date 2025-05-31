ac3549ff2a4420ca5a2ad9c1190438bc162ee849
Malted <malted@malted.dev>
2025-05-31 15:08:41 -0400

    Eager load application on init

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Journey
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("app/models/shop_item")
+    # config.autoload_paths << Rails.root.join("app/models/shop_item")
+    config.after_initialize { eager_load! }
   end
 end
