c0818b4e54e0d933bb3d8f3f1661a5d955b8e8a4
Malted <malted@malted.dev>
2025-06-02 15:31:33 -0400

    Make every shop item field editable

--- app/helpers/shop_items_helper.rb
+++ app/helpers/shop_items_helper.rb
@@ -1,20 +1,37 @@
 module ShopItemsHelper
 
-  # <%= text_field_editable(@item, :name) %>
-  def text_field_editable(item, field_name)
+  def text_field_editable(item, field_name, display_prefix: "")
+    editable(item, field_name, display_prefix, false)
+  end
+
+  def number_field_editable(item, field_name, display_prefix: "")
+    editable(item, field_name, display_prefix, true)
+  end
+
+  private
+
+  def editable(item, field_name, display_prefix = "", is_number)
     container_id = "item-#{item.id}-container-#{field_name}"
+
+    style = "line-clamp-3 text-sm md:text-base 2xl:text-lg text-gray-600 break-words overflow-wrap-anywhere"
+    if field_name == :name
+      style = "text-2xl"
+    end
 
     html = content_tag(:div, id: container_id, class: "item-container") do
       safe_join([
         content_tag(:div, class: "item", style: "display: flex;") do
-          concat content_tag(:p, item.public_send(field_name))
-          admin_tool do
+          raw_value = item.public_send(field_name)
+          value = is_number ? raw_value : raw_value
+          concat content_tag(:p, "#{display_prefix}#{value}", class: style)
+          admin_tool("m-0 px-1 py-0 ml-auto") do
             button_tag "Edit", class: "edit"
           end
         end,
 
         form_with(model: item, html: { style: "display: none;" }) do |f|
-          safe_join([f.text_field(field_name), f.submit])
+          form_field = is_number ? f.number_field(field_name, step: 0.01) : f.text_field(field_name)
+          safe_join([form_field, f.submit])
         end
       ])
     end
