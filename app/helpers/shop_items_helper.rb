c8aa5c362c75bee84af833f1f1c39f6e178ff2c9
Malted <malted@malted.dev>
2025-06-03 12:03:09 -0400

    Improve editable helpers

--- app/helpers/shop_items_helper.rb
+++ app/helpers/shop_items_helper.rb
@@ -1,15 +1,19 @@
 module ShopItemsHelper
   def text_field_editable(item, field_name, display_prefix: "")
-    editable(item, field_name, display_prefix, false)
+    editable(item, field_name, display_prefix, :text)
   end
 
   def number_field_editable(item, field_name, display_prefix: "")
-    editable(item, field_name, display_prefix, true)
+    editable(item, field_name, display_prefix, :number)
+  end
+
+  def image_field_editable(item, field_name)
+    editable(item, field_name, nil, :image)
   end
 
   private
 
-  def editable(item, field_name, display_prefix = "", is_number)
+  def editable(item, field_name, display_prefix = "", field_type)
     container_id = "item-#{item.id}-container-#{field_name}"
 
     style = "line-clamp-3 text-sm md:text-base 2xl:text-lg text-gray-600 break-words overflow-wrap-anywhere"
@@ -19,18 +23,32 @@ module ShopItemsHelper
 
     html = content_tag(:div, id: container_id, class: "item-container") do
       safe_join([
-        content_tag(:div, class: "item", style: "display: flex;") do
-          raw_value = item.public_send(field_name)
-          value = is_number ? raw_value : raw_value
-          concat content_tag(:p, "#{display_prefix}#{value}", class: style)
-          admin_tool("m-0 px-1 py-0 ml-auto") do
-            button_tag "Edit", class: "edit"
+        content_tag(:div, class: "item", style: "display: flex; flex-direction: column;") do
+          if field_type == :image
+            tag = image_tag item.image.variant(:thumb)
+          else
+            tag = content_tag(:p, "#{display_prefix}#{item.public_send(field_name)}", class: style)
+          end
+
+          concat tag
+
+          admin_tool("m-0 my-auto px-1 py-0 ml-auto leading-none") do
+            button_tag "Edit #{field_name}", class: "edit text-xs"
           end
         end,
 
-        form_with(model: item, html: { style: "display: none;" }) do |f|
-          form_field = is_number ? f.number_field(field_name, step: 0.01) : f.text_field(field_name)
-          safe_join([ form_field, f.submit ])
+        form_with(model: item, url: shop_item_path(item), scope: :shop_item, html: { style: "display: none;" }) do |f|
+          if field_type == :text
+            form_field = f.text_field(field_name, class: style)
+          elsif field_type == :number
+            form_field = f.number_field(field_name, step: 0.01, class: style)
+          elsif field_type == :image
+            form_field = f.file_field(field_name, class: style)
+          else
+            raise NotImplementedError
+          end
+
+          safe_join([form_field, f.submit])
         end
       ])
     end
@@ -52,7 +70,7 @@ module ShopItemsHelper
           e.preventDefault();
           const data = new FormData(form);
 
-          fetch("#{url_for(item)}", {
+          fetch("#{shop_item_path(item)}", {
             method: "PATCH",
             body: data,
             headers: {
