3fde826262d2b346096f07a05e2112a342f3c52e
Echo <github@3kh0.net>
2025-06-04 19:19:12 -0400

    carousel deduping and more items (fake)

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -17,6 +17,12 @@ class LandingController < ApplicationController
         image: nil
       },
       {
+        name: "3D Printer Filament Bundle",
+        cost: 40,
+        time: "~40 hours",
+        image: nil
+      },
+      {
         name: "Pinecil Soldering Iron",
         cost: 30,
         time: "~30 hours",
@@ -29,9 +35,33 @@ class LandingController < ApplicationController
         image: nil
       },
       {
+        name: "Cloud Credits - GitHub Codespaces",
+        cost: 75,
+        time: "~75 hours",
+        image: nil
+      },
+      {
+        name: "Cloud Credits - DigitalOcean",
+        cost: 60,
+        time: "~60 hours",
+        image: nil
+      },
+      {
         name: "PCB Credits - JLCPCB",
         cost: 30,
         time: "~30 hours",
+        image: nil
+      },
+      {
+        name: "Electronics Credits - Digikey",
+        cost: 80,
+        time: "~80 hours",
+        image: nil
+      },
+      {
+        name: "Domain Registration Credits",
+        cost: 25,
+        time: "~25 hours",
         image: nil
       },
       {
@@ -41,9 +71,33 @@ class LandingController < ApplicationController
         image: nil
       },
       {
+        name: "Mode Design Sonnet Keyboard",
+        cost: 300,
+        time: "~300 hours",
+        image: nil
+      },
+      {
+        name: "Ben Eater 8-bit Computer Kit",
+        cost: 200,
+        time: "~200 hours",
+        image: nil
+      },
+      {
         name: "Raspberry Pi 5 Starter Kit",
         cost: 90,
         time: "~90 hours",
+        image: nil
+      },
+      {
+        name: "Raspberry Pi Zero 2 W",
+        cost: 35,
+        time: "~35 hours",
+        image: nil
+      },
+      {
+        name: "ThinkPad X1 Carbon (Refurbished)",
+        cost: 600,
+        time: "~600 hours",
         image: nil
       },
       {
@@ -59,21 +113,99 @@ class LandingController < ApplicationController
         image: nil
       },
       {
+        name: "Bose QuietComfort 45",
+        cost: 280,
+        time: "~280 hours",
+        image: nil
+      },
+      {
+        name: "Logitech MX Master 3S Mouse",
+        cost: 80,
+        time: "~80 hours",
+        image: nil
+      },
+      {
+        name: "Logitech Pro X Superlight Mouse",
+        cost: 150,
+        time: "~150 hours",
+        image: nil
+      },
+      {
         name: "Steam Game - Factorio",
         cost: 25,
         time: "~25 hours",
         image: nil
       },
       {
-        name: "Ben Eater 8-bit Computer Kit",
-        cost: 200,
-        time: "~200 hours",
+        name: "Steam Game - Satisfactory",
+        cost: 30,
+        time: "~30 hours",
+        image: nil
+      },
+      {
+        name: "Cricut Explore 3",
+        cost: 180,
+        time: "~180 hours",
         image: nil
       },
       {
         name: "MORE FUDGE!",
         cost: 35,
         time: "~35 hours",
+        image: nil
+      },
+      {
+        name: "Hack Club Merch Bundle",
+        cost: 50,
+        time: "~50 hours",
+        image: nil
+      },
+      {
+        name: "Hack Club Sticker Pack",
+        cost: 10,
+        time: "~10 hours",
+        image: nil
+      },
+      {
+        name: "Signed Photo of Zack Latta",
+        cost: 100,
+        time: "~100 hours",
+        image: nil
+      },
+      {
+        name: "Personal Drawing from MSW",
+        cost: 200,
+        time: "~200 hours",
+        image: nil
+      },
+      {
+        name: "Random Object from HQ",
+        cost: 15,
+        time: "~15 hours",
+        image: nil
+      },
+      {
+        name: "Bottom Half of Chromebook",
+        cost: 25,
+        time: "~25 hours",
+        image: nil
+      },
+      {
+        name: "Mystery eBay Treasure",
+        cost: 30,
+        time: "~30 hours",
+        image: nil
+      },
+      {
+        name: "Top Half of Chromebook",
+        cost: 25,
+        time: "~25 hours",
+        image: nil
+      },
+      {
+        name: "Bottom Half of Chromebook",
+        cost: 30,
+        time: "~30 hours",
         image: nil
       }
     ]
--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["container"];
+  static values = { row: Number };
 
   connect() {
     // me when there is a p2w minecraft server
@@ -9,10 +10,25 @@ export default class extends Controller {
   }
 
   dupertrooper() {
-    const slides = this.containerTarget.children;
-    const count = slides.length;
+    const all = Array.from(this.containerTarget.children);
+    const row = this.rowValue || 1;
+
+    const filtered = all.filter((s, i) => {
+      if (row === 1) {
+        return i % 2 === 0;
+      } else {
+        return i % 2 === 1;
+      }
+    });
+
+    this.containerTarget.innerHTML = "";
+    filtered.forEach((s) => {
+      this.containerTarget.appendChild(s);
+    });
+
+    const count = filtered.length;
     for (let i = 0; i < count; i++) {
-      const clone = slides[i].cloneNode(true);
+      const clone = filtered[i].cloneNode(true);
       this.containerTarget.appendChild(clone);
     }
   }
