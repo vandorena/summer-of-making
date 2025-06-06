d53a1da281cca246041384c9213fbe4cb6b1efd8
Max Wofford <max@maxwofford.com>
2025-06-06 17:04:19 -0400

    Run annotation checks

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@
 #                           slack_callback GET    /auth/slack/callback(.:format)                                                                    sessions#create
 #                             auth_failure GET    /auth/failure(.:format)                                                                           sessions#failure
 #                                   logout DELETE /logout(.:format)                                                                                 sessions#destroy
+#                               magic_link GET    /magic-link(.:format)                                                                             sessions#magic_link
 #                  identity_vault_callback GET    /users/identity_vault_callback(.:format)                                                          users#identity_vault_callback
 #                      link_identity_vault GET    /users/link_identity_vault(.:format)                                                              users#link_identity_vault
 #                                  explore GET    /explore(.:format)                                                                                projects#index
@@ -17,10 +18,10 @@
 #                              my_projects GET    /my_projects(.:format)                                                                            projects#my_projects
 #                               check_link POST   /check_link(.:format)                                                                             projects#check_link
 #                    timer_sessions_active GET    /timer_sessions/active(.:format)                                                                  timer_sessions#global_active
-#                          project_updates POST   /projects/:project_id/updates(.:format)                                                           updates#create
-#                           project_update PATCH  /projects/:project_id/updates/:id(.:format)                                                       updates#update
-#                                          PUT    /projects/:project_id/updates/:id(.:format)                                                       updates#update
-#                                          DELETE /projects/:project_id/updates/:id(.:format)                                                       updates#destroy
+#                          project_devlogs POST   /projects/:project_id/devlogs(.:format)                                                           devlogs#create
+#                           project_devlog PATCH  /projects/:project_id/devlogs/:id(.:format)                                                       devlogs#update
+#                                          PUT    /projects/:project_id/devlogs/:id(.:format)                                                       devlogs#update
+#                                          DELETE /projects/:project_id/devlogs/:id(.:format)                                                       devlogs#destroy
 #            active_project_timer_sessions GET    /projects/:project_id/timer_sessions/active(.:format)                                             timer_sessions#active
 #                   project_timer_sessions POST   /projects/:project_id/timer_sessions(.:format)                                                    timer_sessions#create
 #                    project_timer_session GET    /projects/:project_id/timer_sessions/:id(.:format)                                                timer_sessions#show
@@ -40,7 +41,7 @@
 #                                          PATCH  /projects/:id(.:format)                                                                           projects#update
 #                                          PUT    /projects/:id(.:format)                                                                           projects#update
 #                                          DELETE /projects/:id(.:format)                                                                           projects#destroy
-#                                  updates GET    /updates(.:format)                                                                                updates#index
+#                                  devlogs GET    /devlogs(.:format)                                                                                devlogs#index
 #                                    votes POST   /votes(.:format)                                                                                  votes#create
 #                                 new_vote GET    /votes/new(.:format)                                                                              votes#new
 #                               shop_items POST   /shop_items(.:format)                                                                             shop_items#create
@@ -57,27 +58,27 @@
 #                                                 /400(.:format)                                                                                    errors#bad_request
 #                       upload_attachments POST   /attachments/upload(.:format)                                                                     attachments#upload
 #                     download_attachments GET    /attachments/download(.:format)                                                                   attachments#download
-#                          update_comments POST   /updates/:update_id/comments(.:format)                                                            comments#create
-#                           update_comment DELETE /updates/:update_id/comments/:id(.:format)                                                        comments#destroy
-#                       toggle_like_update POST   /updates/:id/toggle_like(.:format)                                                                likes#toggle
-#                                          GET    /updates(.:format)                                                                                updates#index
-#                                          POST   /updates(.:format)                                                                                updates#create
-#                               new_update GET    /updates/new(.:format)                                                                            updates#new
-#                              edit_update GET    /updates/:id/edit(.:format)                                                                       updates#edit
-#                                   update GET    /updates/:id(.:format)                                                                            updates#show
-#                                          PATCH  /updates/:id(.:format)                                                                            updates#update
-#                                          PUT    /updates/:id(.:format)                                                                            updates#update
-#                                          DELETE /updates/:id(.:format)                                                                            updates#destroy
+#                          devlog_comments POST   /devlogs/:devlog_id/comments(.:format)                                                            comments#create
+#                           devlog_comment DELETE /devlogs/:devlog_id/comments/:id(.:format)                                                        comments#destroy
+#                       toggle_like_devlog POST   /devlogs/:id/toggle_like(.:format)                                                                likes#toggle
+#                                          GET    /devlogs(.:format)                                                                                devlogs#index
+#                                          POST   /devlogs(.:format)                                                                                devlogs#create
+#                               new_devlog GET    /devlogs/new(.:format)                                                                            devlogs#new
+#                              edit_devlog GET    /devlogs/:id/edit(.:format)                                                                       devlogs#edit
+#                                   devlog GET    /devlogs/:id(.:format)                                                                            devlogs#show
+#                                          PATCH  /devlogs/:id(.:format)                                                                            devlogs#update
+#                                          PUT    /devlogs/:id(.:format)                                                                            devlogs#update
+#                                          DELETE /devlogs/:id(.:format)                                                                            devlogs#destroy
 #                   tutorial_complete_step POST   /tutorial/complete_step(.:format)                                                                 tutorial_progress#complete_step
 #                          api_v1_projects GET    /api/v1/projects(.:format)                                                                        api/v1/projects#index
 #                           api_v1_project GET    /api/v1/projects/:id(.:format)                                                                    api/v1/projects#show
-#                           api_v1_updates GET    /api/v1/updates(.:format)                                                                         api/v1/updates#index
-#                            api_v1_update GET    /api/v1/updates/:id(.:format)                                                                     api/v1/updates#show
+#                           api_v1_devlogs GET    /api/v1/devlogs(.:format)                                                                         api/v1/devlogs#index
+#                            api_v1_devlog GET    /api/v1/devlogs/:id(.:format)                                                                     api/v1/devlogs#show
 #                          api_v1_comments GET    /api/v1/comments(.:format)                                                                        api/v1/comments#index
 #                           api_v1_comment GET    /api/v1/comments/:id(.:format)                                                                    api/v1/comments#show
 #                             api_v1_emote GET    /api/v1/emotes/:id(.:format)                                                                      api/v1/emotes#show
 #                           api_check_user GET    /api/check_user(.:format)                                                                         users#check_user
-#                              api_updates POST   /api/updates(.:format)                                                                            updates#api_create
+#                              api_devlogs POST   /api/devlogs(.:format)                                                                            devlogs#api_create
 #      users_update_hackatime_confirmation POST   /users/update_hackatime_confirmation(.:format)                                                    users#update_hackatime_confirmation
 #                  users_refresh_hackatime POST   /users/refresh_hackatime(.:format)                                                                users#refresh_hackatime
 #         users_check_hackatime_connection POST   /users/check_hackatime_connection(.:format)                                                       users#check_hackatime_connection
@@ -145,6 +146,15 @@
 #                                     PATCH  /resources/shop_items/:id(.:format)                                                                avo/shop_items#update
 #                                     PUT    /resources/shop_items/:id(.:format)                                                                avo/shop_items#update
 #                                     DELETE /resources/shop_items/:id(.:format)                                                                avo/shop_items#destroy
+#        preview_resources_magic_link GET    /resources/magic_links/:id/preview(.:format)                                                       avo/magic_links#preview
+#               resources_magic_links GET    /resources/magic_links(.:format)                                                                   avo/magic_links#index
+#                                     POST   /resources/magic_links(.:format)                                                                   avo/magic_links#create
+#            new_resources_magic_link GET    /resources/magic_links/new(.:format)                                                               avo/magic_links#new
+#           edit_resources_magic_link GET    /resources/magic_links/:id/edit(.:format)                                                          avo/magic_links#edit
+#                resources_magic_link GET    /resources/magic_links/:id(.:format)                                                               avo/magic_links#show
+#                                     PATCH  /resources/magic_links/:id(.:format)                                                               avo/magic_links#update
+#                                     PUT    /resources/magic_links/:id(.:format)                                                               avo/magic_links#update
+#                                     DELETE /resources/magic_links/:id(.:format)                                                               avo/magic_links#destroy
 #          resources_associations_new GET    /resources/:resource_name/:id/:related_name/new(.:format)                                          avo/associations#new
 #        resources_associations_index GET    /resources/:resource_name/:id/:related_name(.:format)                                              avo/associations#index
 #         resources_associations_show GET    /resources/:resource_name/:id/:related_name/:related_id(.:format)                                  avo/associations#show
