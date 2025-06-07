ef9abd1cd114d72c496dc19aabbb20c4ca66ba84
Kartikey Chauhan <me@cskartikey.dev>
2025-06-07 11:44:48 +0530

    linter

--- app/avo/resources/email_signup.rb
+++ app/avo/resources/email_signup.rb
@@ -4,7 +4,7 @@ class Avo::Resources::EmailSignup < Avo::BaseResource
   # self.search = {
   #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
   # }
-  
+
   def fields
     field :id, as: :id
     field :email, as: :text
