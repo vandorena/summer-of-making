29fdd5ee386170929162410d7cc02addec1d11a1
Malted <malted@malted.dev>
2025-06-08 19:29:18 -0400

    ShipEvent user and devlogs_since_last method implementations

--- app/models/ship_event.rb
+++ app/models/ship_event.rb
@@ -17,4 +17,23 @@
 #
 class ShipEvent < ApplicationRecord
   belongs_to :project
+
+  def user
+    project.user
+  end
+
+  def devlogs_since_last
+    previous_ships = ShipEvent.where(project: project)
+                              .where("created_at < ?", created_at)
+                              .order(:created_at)
+
+
+    if previous_ships.empty?
+      Devlog.where(project: project).where("created_at < ?", created_at)
+    else
+      last_ship_date = previous_ships.last.created_at
+      Devlog.where(project: project)
+            .where("created_at > ? AND created_at < ?", last_ship_date, created_at)
+    end
+  end
 end
