0b7e1c1fcc16e89ccc7a5b7e1bcf22221d6ef648
Kartikey Chauhan <me@cskartikey.dev>
2025-06-10 10:30:54 +0530

    linters

--- app/controllers/payouts_controller.rb
+++ app/controllers/payouts_controller.rb
@@ -5,11 +5,11 @@ class PayoutsController < ApplicationController
     respond_to do |format|
       format.json do
         user = User.find_by(slack_id: params[:slack_id])
-        return render json: { error: 'User not found' }, status: :not_found unless user
-        
+        return render json: { error: "User not found" }, status: :not_found unless user
+
         render json: {
           balance: user.balance,
-          payouts: user.payouts,
+          payouts: user.payouts
         }
       end
       format.html { @payouts = current_user.payouts }
@@ -21,7 +21,7 @@ class PayoutsController < ApplicationController
 
     render json: {
       balance: user.balance,
-      payouts: user.payouts,
+      payouts: user.payouts
     }
   end
 
@@ -29,7 +29,7 @@ class PayoutsController < ApplicationController
 
   def verify_fraud_token
     unless params[:token] == Rails.application.credentials.fraud.token
-      render json: { error: 'Unauthorized' }, status: :unauthorized
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
   end
 end
