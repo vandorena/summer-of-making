98ffd3513f5e16a583715f3bc5ef611fa7b4fb1c
Echo <github@3kh0.net>
2025-06-09 20:24:19 -0400

    rework nav bar #82

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,126 +1,43 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [
-    "sidebar",
-    "content",
-    "collapseIcon",
-    "icon",
-    "avatar",
-    "logoutContainer",
-    "topContainer",
-    "mainContent",
-    "collapsedOverlay",
-  ];
+  static targets = ["collapseIcon"];
+  static values = {
+    collapsed: { type: Boolean, default: false },
+  };
 
   connect() {
-    this.expanded = true;
-
-    // Store sidebar state in localStorage to persist across page loads (exists cause I don't want state to reset when user clicks on another button)
-    if (localStorage.getItem("sidebarCollapsed") === "true") {
-      this.collapse();
-      this.expanded = false;
+    const s = localStorage.getItem("sidebarCollapsed");
+    if (s === "true") {
+      this.collapsedValue = true;
       if (this.hasCollapseIconTarget) {
         this.collapseIconTarget.classList.add("rotate-180");
       }
-    } else {
-      if (this.hasCollapsedOverlayTarget) {
-        this.collapsedOverlayTarget.classList.add("hidden");
-      }
     }
+    this.u();
   }
 
-  disconnect() {}
-
-  toggle(event) {
-    if (event) {
-      event.stopPropagation();
+  toggle(e) {
+    if (e) {
+      e.stopPropagation();
     }
-
-    if (this.expanded) {
-      this.collapse();
-    } else {
-      this.expand();
-    }
-    this.expanded = !this.expanded;
-
-    localStorage.setItem("sidebarCollapsed", !this.expanded);
-
-    // Rotate. I'm not asking darlene to design another icon :D
+    this.collapsedValue = !this.collapsedValue;
+    localStorage.setItem("sidebarCollapsed", this.collapsedValue);
     if (this.hasCollapseIconTarget) {
-      this.collapseIconTarget.classList.toggle("rotate-180");
+      this.collapseIconTarget.classList.toggle(
+        "rotate-180",
+        this.collapsedValue
+      );
     }
   }
 
-  collapse() {
-    this.sidebarTarget.classList.add("collapsed");
-    this.contentTargets.forEach((element) => {
-      element.classList.add("hidden");
-    });
-
-    // Hide icons but keep avatar visible when collapsed
-    this.iconTargets.forEach((element) => {
-      element.classList.add("hidden");
-    });
-
-    // Don't hide the avatar when collapsed - we want to show just the profile picture
-    // if (this.hasAvatarTarget) {
-    //   this.avatarTarget.classList.add("hidden");
-    // }
-
-    // Centre the top container
-    if (this.hasTopContainerTarget) {
-      this.topContainerTarget.classList.add("justify-center");
-      this.topContainerTarget.classList.remove("justify-between");
-    }
-
-    // Centre the logout container
-    if (this.hasLogoutContainerTarget) {
-      this.logoutContainerTarget.classList.add("justify-center");
-      this.logoutContainerTarget.classList.remove("justify-between");
-    }
-
-    // Adjust main content margin when sidebar is collapsed
-    if (this.hasMainContentTarget) {
-      this.mainContentTarget.classList.remove("ml-64", "lg:ml-74", "2xl:ml-96");
-      this.mainContentTarget.classList.add("ml-32");
-    }
+  collapsedValueChanged() {
+    this.u();
   }
 
-  expand() {
-    this.sidebarTarget.classList.remove("collapsed");
-    this.contentTargets.forEach((element) => {
-      element.classList.remove("hidden");
+  u() {
+    document.querySelectorAll("[data-sidebar-collapsed]").forEach((e) => {
+      e.setAttribute("data-sidebar-collapsed", this.collapsedValue);
     });
-
-    this.iconTargets.forEach((element) => {
-      element.classList.remove("hidden");
-    });
-
-    // Avatar is already visible, no need to explicitly show it
-    // if (this.hasAvatarTarget) {
-    //   this.avatarTarget.classList.remove("hidden");
-    // }
-
-    // Hide collapsed overlay when expanded
-    if (this.hasCollapsedOverlayTarget) {
-      this.collapsedOverlayTarget.classList.add("hidden");
-      this.collapsedOverlayTarget.classList.remove("hidden");
-    }
-
-    if (this.hasTopContainerTarget) {
-      this.topContainerTarget.classList.remove("justify-center");
-      this.topContainerTarget.classList.add("justify-between");
-    }
-
-    if (this.hasLogoutContainerTarget) {
-      this.logoutContainerTarget.classList.remove("justify-center");
-      this.logoutContainerTarget.classList.add("justify-between");
-    }
-
-    if (this.hasMainContentTarget) {
-      this.mainContentTarget.classList.remove("ml-32");
-      this.mainContentTarget.classList.add("ml-64", "lg:ml-74", "2xl:ml-96");
-    }
   }
 }
