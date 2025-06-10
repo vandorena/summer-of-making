7179ca8c41ec7d6b1183a5522dcf03f7ebd544da
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-06-10 00:21:14 +0000

    feat: really proud of this sidebar 🧑‍🍳

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,11 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
+// After how many milliseconds should we collapse the sidebar when the user is not hovering
+// over it?
+const COLLAPSE_DELAY = 250;
+
+// For how many milliseconds should elements marked with 'collapseFade' fade out for?
+// This should be a value recognized by Tailwind.
+const FADE_DURATION = "200";
+
 export default class extends Controller {
-  static targets = ["sidebar", "content", "collapseIcon", "icon", "avatar", "logoutContainer", "topContainer", "mainContent"]
-  
+  static targets = [
+    "sidebar",
+    "content",
+    "collapseIcon",
+    "icon",
+    "avatar",
+    "logoutContainer",
+    "topContainer",
+    "mainContent",
+    "collapseHide",
+    "collapseFade",
+    "underline",
+    "links"
+  ]
+
   connect() {
     this.expanded = true
-    
+    this.transitioning = false
+    this.mouseEntered = false
+    this.prevKnownSize = this.hasSidebarTarget ? this.sidebarTarget.clientWidth + "px" : "100px";
+
+    this.collapseFadeTargets.forEach(element => {
+      element.classList.add("transition-opacity", `duration-${FADE_DURATION}`)
+    })
+  
     // Store sidebar state in localStorage to persist across page loads (exists cause I don't want state to reset when user clicks on another button)
     if (localStorage.getItem('sidebarCollapsed') === 'true') {
       this.collapse()
@@ -18,6 +46,42 @@ export default class extends Controller {
   
   disconnect() {
   }
+
+  /**
+   * @param {MouseEvent} event 
+   */
+  handleMouseEnter(event) {
+    if (this.transitioning) {
+      console.log("'handleMouseEnter' invoked while playing the sidebar transition animation. Ignoring.");
+      return;
+    }
+
+    // As soon as the user touches the sidebar, expand it.
+    if (!this.expanded) {
+      this.expand()
+    }
+
+    this.mouseEntered = true
+  }
+
+  /**
+   * @param {MouseEvent} event 
+   */
+  handleMouseExit(event) {
+    if (this.transitioning) {
+      console.log("'handleMouseExit' invoked while playing the sidebar transition animation. Ignoring.");
+      return;
+    }
+
+    this.mouseEntered = false
+
+    setTimeout(() => {
+      // If after .5s the user hasn't moved the mouse back inside the sidebar, hide it.
+      if (!this.mouseEntered) {
+        this.collapse()
+      }
+    }, COLLAPSE_DELAY)
+  }
   
   toggle(event) {
     if (event) {
@@ -29,7 +93,6 @@ export default class extends Controller {
     } else {
       this.expand()
     }
-    this.expanded = !this.expanded
     
     localStorage.setItem('sidebarCollapsed', !this.expanded)
     
@@ -40,66 +103,75 @@ export default class extends Controller {
   }
   
   collapse() {
-    this.sidebarTarget.classList.add("collapsed")
-    this.contentTargets.forEach(element => {
-      element.classList.add("hidden")
-    })
-    
-    // Hide icons and avatar
-    this.iconTargets.forEach(element => {
-      element.classList.add("hidden")
-    })
-    
-    if (this.hasAvatarTarget) {
-      this.avatarTarget.classList.add("hidden")
-    }
-    
-    // Centre the top container
-    if (this.hasTopContainerTarget) {
-      this.topContainerTarget.classList.add("justify-center")
-      this.topContainerTarget.classList.remove("justify-between")
-    }
-    
-    // Centre the logout container
-    if (this.hasLogoutContainerTarget) {
-      this.logoutContainerTarget.classList.add("justify-center")
-      this.logoutContainerTarget.classList.remove("justify-between")
-    }
+    // Each element that needs to be hidden when the sidebar is collapsed should
+    // be marked with the "collapseHide" target. 
+    //
+    // We want a nice width scaling animation, but CSS can realistically only do that
+    // when the property actually *changes*... if `width` is 100% and something changes
+    // inside the element, it doesn't animate, unfortunately.
+    //
+    // When collapsing the sidebar, we calculate its expanded width. We assign that
+    // width as a fixed one in CSS (hopefully not causing any layout shifting in the process),
+    // and then, one animation frame later, we set the width to a known, expected value.
+    this.transitioning = true;
 
-    // Adjust main content margin when sidebar is collapsed
-    if (this.hasMainContentTarget) {
-      this.mainContentTarget.classList.remove("ml-64", "lg:ml-74", "2xl:ml-96")
-      this.mainContentTarget.classList.add("ml-32")
+    this.sidebarTarget.style.width = this.prevKnownSize = `${this.sidebarTarget.clientWidth}px`;
+    setTimeout(() => {
+      this.sidebarTarget.style.width = "48px";
+      this.transitioning = false;
+    }, 16);
+
+    this.sidebarTarget.classList.add("collapsed")
+    this.collapseHideTargets.forEach(element => {
+      element.classList.add("hidden")
+    })
+
+    this.collapseFadeTargets.forEach(element => {
+      element.classList.add("opacity-0")
+    })
+
+    this.underlineTargets.forEach(element => {
+      element.classList.remove("w-full")
+      element.classList.add("w-[36px]")
+      element.style.transform = "translateX(-10px)"
+    })
+
+    if (this.hasLinksTarget) {
+      this.linksTarget.style.transform = "translateX(5px)";
     }
+  
+    this.expanded = false
   }
   
   expand() {
+    this.transitioning = true;
+
+    this.sidebarTarget.style.width = this.prevKnownSize;
+    setTimeout(() => {
+      this.sidebarTarget.style.width = "";
+      this.transitioning = false;
+    }, 16);
+
     this.sidebarTarget.classList.remove("collapsed")
-    this.contentTargets.forEach(element => {
+    this.sidebarTarget.classList.remove("w-[30px]")
+    this.collapseHideTargets.forEach(element => {
       element.classList.remove("hidden")
     })
-    
-    this.iconTargets.forEach(element => {
-      element.classList.remove("hidden")
+
+    this.collapseFadeTargets.forEach(element => {
+      element.classList.remove("opacity-0")
     })
-    
-    if (this.hasAvatarTarget) {
-      this.avatarTarget.classList.remove("hidden")
+
+    this.underlineTargets.forEach(element => {
+      element.classList.add("w-full")
+      element.classList.remove("w-[36px]")
+      element.style.transform = "";
+    })
+
+    if (this.hasLinksTarget) {
+      this.linksTarget.style.transform = "";
     }
-    
-    if (this.hasTopContainerTarget) {
-      this.topContainerTarget.classList.remove("justify-center")
-      this.topContainerTarget.classList.add("justify-between")
-    }
-    
-    if (this.hasLogoutContainerTarget) {
-      this.logoutContainerTarget.classList.remove("justify-center")
-      this.logoutContainerTarget.classList.add("justify-between")
-    }
-    
-    if (this.hasMainContentTarget) {
-      this.mainContentTarget.classList.remove("ml-32")
-      this.mainContentTarget.classList.add("ml-64", "lg:ml-74", "2xl:ml-96")
-    }
+
+    this.expanded = true
   }
 } 
