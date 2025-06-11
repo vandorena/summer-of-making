2b936c591295e687d1d7eb506e009c72a6115b02
Malted <malted@malted.dev>
2025-06-10 23:52:13 -0400

    Pundit authorization in appl ctrlr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   include PublicActivity::StoreController
+  include Pundit::Authorization
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action do
