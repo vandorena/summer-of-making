d142457b470d2495d42e1290520482d934a4a06d
Neon <neon@saahild.com>
2025-06-14 02:05:23 -0400

    Neon/idkgit (#215)
    * fix: only change req stuff
    
    * fix: again

--- app/models/user.rb
+++ app/models/user.rb
@@ -295,7 +295,7 @@ class User < ApplicationRecord
   end
 
   def notify_xyz_on_verified
-    if saved_change_to_ysws_verified? && ysws_verified
+    if  ysws_verified
       begin
         uri = URI.parse("https://webhook.site/c6889c91-08c3-46d6-8ff0-29c96ab54b23")
         Net::HTTP.post_form(uri, { email: email })
