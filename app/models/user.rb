3840bb26f203bcc76db6698219bf53788b8b965a
Max Wofford <max@maxwofford.com>
2025-06-16 08:50:03 -0400

    Add default start time to hackatime calls

--- app/models/user.rb
+++ app/models/user.rb
@@ -112,7 +112,10 @@ class User < ApplicationRecord
   end
 
   def self.check_hackatime(slack_id)
-    response = Faraday.get("https://hackatime.hackclub.com/api/v1/users/#{slack_id}/stats?features=projects")
+    start_date = Time.use_zone("America/New_York") do
+      Time.parse("2025-06-16").beginning_of_day
+    end
+    response = Faraday.get("https://hackatime.hackclub.com/api/v1/users/#{slack_id}/stats?features=projects&start_date=#{start_date}")
     result = JSON.parse(response.body)&.dig("data")
     return unless result["status"] == "ok"
 
@@ -159,7 +162,10 @@ class User < ApplicationRecord
   end
 
   def refresh_hackatime_data_now
-    response = Faraday.get("https://hackatime.hackclub.com/api/v1/users/#{slack_id}/stats?features=projects")
+    start_date = Time.use_zone("America/New_York") do
+      Time.parse("2025-06-16").beginning_of_day
+    end
+    response = Faraday.get("https://hackatime.hackclub.com/api/v1/users/#{slack_id}/stats?features=projects&start_date=#{start_date}")
     return unless response.success?
 
     result = JSON.parse(response.body)
