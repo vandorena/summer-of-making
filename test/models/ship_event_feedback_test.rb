44c3af43acba06cf8347177a894da31d47d9cf7b
Christopher Walker <15720465+polytroper@users.noreply.github.com>
2025-06-20 14:14:33 -0400

    terminal stuff

--- test/models/ship_event_feedback_test.rb
+++ test/models/ship_event_feedback_test.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: ship_event_feedbacks
+#
+#  id            :bigint           not null, primary key
+#  comment       :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  ship_event_id :bigint           not null
+#
+# Indexes
+#
+#  index_ship_event_feedbacks_on_ship_event_id  (ship_event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ship_event_id => ship_events.id)
+#
 require "test_helper"
 
 class ShipEventFeedbackTest < ActiveSupport::TestCase
