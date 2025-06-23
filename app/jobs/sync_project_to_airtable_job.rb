ea5a111b2599cb4b98c5b400b239d30cbb3d8db2
Malted <malted@malted.dev>
2025-06-23 12:19:03 -0400

    Remove project banner from airtable sync job

--- app/jobs/sync_project_to_airtable_job.rb
+++ app/jobs/sync_project_to_airtable_job.rb
@@ -16,7 +16,6 @@ class SyncProjectToAirtableJob < ApplicationJob
       "repo_link" => project.repo_link,
       "readme_link" => project.readme_link,
       "demo_link" => project.demo_link,
-      "banner_link" => project.banner,
       "category" => project.category,
       "author_slack_id" => author_slack_id,
       "project_id" => project.id.to_s,
@@ -29,7 +28,7 @@ class SyncProjectToAirtableJob < ApplicationJob
 
     if existing_record
       updated = false
-      %w[title description repo_link readme_link demo_link banner_link category author_slack_id project_id
+      %w[title description repo_link readme_link demo_link category author_slack_id project_id
          is_shipped].each do |field|
         new_value = project_data[field]
         if existing_record[field] != new_value
