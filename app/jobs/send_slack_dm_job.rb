3e3fab1f3ed95656b13c41e0a21afb0227e2d916
Max Wofford <max@maxwofford.com>
2025-06-24 15:17:05 -0400

    Add basic caching to send_slack_dm_job

--- app/jobs/send_slack_dm_job.rb
+++ app/jobs/send_slack_dm_job.rb
@@ -6,8 +6,10 @@ class SendSlackDmJob < ApplicationJob
   def perform(user_id, message)
     client = Slack::Web::Client.new(token: ENV.fetch("SLACK_BOT_TOKEN", nil))
 
-    response = client.conversations_open(users: user_id)
-    channel_id = response.channel.id
+    channel_id = Rails.cache.fetch("slack_channel_id_#{user_id}", expires_in: 1.hour) do
+      response = client.conversations_open(users: user_id)
+      response.channel.id
+    end
 
     client.chat_postMessage(
       channel: channel_id,
