685bab59f1537363b95acb7400dd631c26936c66
Malted <malted@malted.dev>
2025-06-25 16:35:58 -0400

    Dedupe email signups

--- app/models/email_signup.rb
+++ app/models/email_signup.rb
@@ -16,4 +16,15 @@
 #  index_email_signups_on_email  (email) UNIQUE
 #
 class EmailSignup < ApplicationRecord
+  # def self.dedupe
+  #   # find all models and group them on keys which should be common
+  #   grouped = all.group_by{|model| [model.email] }
+  #   grouped.values.each do |duplicates|
+  #     # the first one we want to keep right?
+  #     first_one = duplicates.shift # or pop for last one
+  #     # if there are any more left, they are duplicates
+  #     # so delete all of them
+  #     duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+  #   end
+  # end
 end
