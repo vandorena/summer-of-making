c3a7339ed6c6e1f8b118c474c7d7089dd9f22765
Malted <malted@malted.dev>
2025-06-25 15:44:23 -0400

    Refactor RefreshHackatimeStatsJob to use user.fetch_raw_hackatime_stats to hit hackatime /stats

--- app/jobs/refresh_hackatime_stats_job.rb
+++ app/jobs/refresh_hackatime_stats_job.rb
@@ -7,12 +7,7 @@ class RefreshHackatimeStatsJob < ApplicationJob
     user = User.find_by(id: user_id)
     return unless user&.has_hackatime
 
-    start_date = Time.use_zone("America/New_York") do
-      Time.parse("2025-06-16").beginning_of_day
-    end
-    uri = URI("https://hackatime.hackclub.com/api/v1/users/#{user.slack_id}/stats?features=projects&start_date=#{start_date}")
-
-    response = Faraday.get(uri.to_s)
+    response = user.fetch_raw_hackatime_stats
     return unless response.success?
 
     result = JSON.parse(response.body)
