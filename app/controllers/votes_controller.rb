c67609aeed1b84b8bdce84bb8e6dfca1ff159466
Kartikey Chauhan <me@cskartikey.dev>
2025-06-27 21:44:07 +0530

    better hardcode :pf:

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -96,13 +96,13 @@ class VotesController < ApplicationController
                                       .distinct
                                       .pluck("ship_events.id")
 
-    @projects = [Project.find(1), Project.find(4)]
+    @projects = Project.find([1, 4])
 
     # projects_with_latest_ship = Project
     #                               .joins(:ship_events)
     #                               .joins(:ship_certifications)
     #                               .includes(:user, :banner_attachment,
-    #                                         devlogs: [ :user, :file_attachment ])
+    #                                        devlogs: [ :user, :file_attachment ])
     #                               .where(ship_certifications: { judgement: :approved })
     #                               .where.not(user_id: current_user.id)
     #                               .where(
@@ -122,19 +122,60 @@ class VotesController < ApplicationController
 
     # eligible_projects = projects_with_latest_ship.to_a
 
-    # two projects by different authors
+    #   projects_with_time = eligible_projects.map do |project|
+    #     latest_ship_event = project.ship_events.order(:created_at).last
+
+    #     ship_devlogs = latest_ship_event.devlogs_since_last
+    #                                    .where("created_at < ?", latest_ship_event.created_at)
+    #     total_time_seconds = ship_devlogs.sum(:last_hackatime_time)
+
+    #   {
+    #     project: project,
+    #     total_time: total_time_seconds
+    #   }
+    # end
+
+    # if projects_with_time.size < 2
+    #   @projects = []
+    #   return
+    # end
+
     # selected_projects = []
     # used_user_ids = Set.new
+    # max_attempts = 25 # infinite loop!
 
-    # eligible_projects.shuffle!
+    # attempts = 0
+    # while selected_projects.size < 2 && attempts < max_attempts
+    #   attempts += 1
 
-    # eligible_projects.each do |project|
-    #   next if used_user_ids.include?(project.user_id)
+    #   # pick a raqndom project and get smth in it's range
+    #   if selected_projects.empty?
+    #     first_project_data = projects_with_time.select { |p| !used_user_ids.include?(p[:project].user_id) }.sample
+    #     next unless first_project_data
 
-    #   selected_projects << project
-    #   used_user_ids << project.user_id
+    #     selected_projects << first_project_data[:project]
+    #     used_user_ids << first_project_data[:project].user_id
+    #     first_time = first_project_data[:total_time]
 
-    #   break if selected_projects.size == 2
+    #     # find projects within the constraints (set to 30%)
+    #     min_time = first_time * 0.7
+    #     max_time = first_time * 1.3
+
+    #     compatible_projects = projects_with_time.select do |p|
+    #       !used_user_ids.include?(p[:project].user_id) &&
+    #       p[:total_time] >= min_time &&
+    #       p[:total_time] <= max_time
+    #     end
+
+    #     if compatible_projects.any?
+    #       second_project_data = compatible_projects.sample
+    #       selected_projects << second_project_data[:project]
+    #       used_user_ids << second_project_data[:project].user_id
+    #     else
+    #       selected_projects.clear
+    #       used_user_ids.clear
+    #     end
+    #   end
     # end
 
     # if selected_projects.size < 2
@@ -143,6 +184,9 @@ class VotesController < ApplicationController
     # end
 
     # @projects = selected_projects
+    # @ship_events = selected_projects.map do |project|
+    #   project.ship_events.order(:created_at).last
+    # end
   end
 
   def vote_params
