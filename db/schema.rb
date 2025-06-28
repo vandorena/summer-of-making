f4b8106b15824928655f5bba94f71af53cdfed8e
Max Wofford <max@maxwofford.com>
2025-06-28 17:14:02 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -227,6 +227,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_28_190014) do
     t.index ["email"], name: "index_email_signups_on_email", unique: true
   end
 
+  create_table "flipper_features", force: :cascade do |t|
+    t.string "key", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_flipper_features_on_key", unique: true
+  end
+
+  create_table "flipper_gates", force: :cascade do |t|
+    t.string "feature_key", null: false
+    t.string "key", null: false
+    t.text "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_key", "key", "value"], name: "index_flipper_gates_on_feature_key_and_key_and_value", unique: true
+  end
+
   create_table "hackatime_projects", force: :cascade do |t|
     t.string "name"
     t.integer "seconds"
