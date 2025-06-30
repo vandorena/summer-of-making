f7f3f23e6b7e552128b293da82ae36023192dc98
Malted <malted@malted.dev>
2025-06-30 15:34:01 -0700

    Quote shipevent type

--- app/jobs/one_time/initiate_genesis_payouts_job.rb
+++ app/jobs/one_time/initiate_genesis_payouts_job.rb
@@ -2,7 +2,7 @@ class OneTime::InitiateGenesisPayoutsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    return if Payout.where(payable_type: ShipEvent).any? # Protect from running twice
+    return if Payout.where(payable_type: "ShipEvent").any? # Protect from running twice
 
     Project.find_each(&:issue_payouts)
   end
