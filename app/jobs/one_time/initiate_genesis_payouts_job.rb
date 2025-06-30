e60c9912b108fb5b91d832e6593e0f6376eb4061
Malted <malted@malted.dev>
2025-06-30 13:27:39 -0700

    Protect InitiateGenesisPayoutsJob from running more than one time

--- app/jobs/one_time/initiate_genesis_payouts_job.rb
+++ app/jobs/one_time/initiate_genesis_payouts_job.rb
@@ -2,6 +2,8 @@ class OneTime::InitiateGenesisPayoutsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    return if Payout.where(payable_type: ShipEvent).any? # Protect from running twice
+
     genesis_projects.each do |p|
       p.issue_payouts
     end
