af92383e4283ecf825273cf48afd6ad9e924eaec
Malted <malted@malted.dev>
2025-06-30 13:37:10 -0700

    Simplify genesis job

--- app/jobs/one_time/initiate_genesis_payouts_job.rb
+++ app/jobs/one_time/initiate_genesis_payouts_job.rb
@@ -4,17 +4,6 @@ class OneTime::InitiateGenesisPayoutsJob < ApplicationJob
   def perform(*args)
     return if Payout.where(payable_type: ShipEvent).any? # Protect from running twice
 
-    genesis_projects.each do |p|
-      p.issue_payouts
-    end
-  end
-
-  private
-
-  def genesis_projects
-    Project
-      .joins(:ship_events)
-      .group("projects.id")
-      .having("MAX(ship_events.created_at) < ?", Vote::VOTING_START_DATE)
+    Project.find_each(:issue_payouts)
   end
 end
