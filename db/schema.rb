23e78050c90441e4a6184037b38a03d34e17cdb1
Malted <malted@malted.dev>
2025-07-17 16:45:54 -0400

    Resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema[8.0].define(version: 2025_07_14_191309) do
-=======
 ActiveRecord::Schema[8.0].define(version: 2025_07_17_203326) do
->>>>>>> Stashed changes
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
