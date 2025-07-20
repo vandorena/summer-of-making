02e0e04aca1b016169693bbb95c83483cd6d08b1
Echo <github@3kh0.net>
2025-07-19 20:11:54 -0400

    report text validation (i see you toshit)

--- app/controllers/fraud_reports_controller.rb
+++ app/controllers/fraud_reports_controller.rb
@@ -9,6 +9,12 @@ class FraudReportsController < ApplicationController
       **fraud_report_params
     )
 
+    unless valid_report(fraud_report)
+      flash[:alert] = "nice try silly!"
+      redirect_to request.referer || root_path
+      return
+    end
+
     if fraud_report.save
       flash[:notice] = "Thank you for reporting this. We'll investigate."
       redirect_to request.referer || root_path
@@ -31,5 +37,39 @@ class FraudReportsController < ApplicationController
 
   def fraud_report_params
     params.require(:fraud_report).permit(:suspect_type, :suspect_id, :reason)
+  end
+
+  def valid_report(fraud_report)
+    reason = fraud_report.reason
+
+    return false if reason.blank? || reason.length < 20
+    return false if reason.length > 1000
+    sr = ActionController::Base.helpers.strip_tags(reason)
+    fraud_report.reason = sr.strip
+
+    sussy = [
+      /<script/i,
+      /javascript:/i,
+      /on\w+\s*=/i,
+      /<iframe/i,
+      /<object/i,
+      /<embed/i,
+      /<form/i,
+      /data:text\/html/i,
+      /vbscript:/i,
+      /<meta/i
+    ]
+
+    sussy.each do |pattern|
+      return false if reason.match?(pattern)
+    end
+
+    sc = reason.scan(/[<>{}()\[\]"'`]/).length
+    return false if sc > 10
+
+    ac = reason.scan(/[a-zA-Z0-9\s]/).length
+    return false if ac < (reason.length * 0.7)
+
+    true
   end
 end
