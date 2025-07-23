1e7b53ff3059d8d274f241bab4dd6312722473de
Kartikey Chauhan <me@cskartikey.dev>
2025-07-24 00:05:30 +0530

    feat: implement slack warning if unlogged time exceeds threshold

--- config/initializers/hackatime_job.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.after_initialize do
-  if defined?(Rails::Server) && Rails.env.production?
-    UserHackatimeDataRefreshJob.perform_later
-  end
-end
