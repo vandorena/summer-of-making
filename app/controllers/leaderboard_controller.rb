59cb8f3971d5ae718be8ac4d7f406b7ae6b4c0ae
Echo <github@3kh0.net>
2025-07-25 17:04:39 -0400

    bing bong banu (#762)

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -4,13 +4,17 @@ class LeaderboardController < ApplicationController
     @users = User.order(Arel.sql("COALESCE((SELECT SUM(amount) FROM payouts WHERE payouts.user_id = users.id), 0) DESC")).limit(50)
 
     if current_user
-      c = current_user.balance
+      current_shells = current_user.balance
       @current_pos = User.where(
         Arel.sql("COALESCE((SELECT SUM(amount) FROM payouts WHERE payouts.user_id = users.id), 0) > ?"),
-        c
+        current_shells
       ).count + 1
     end
 
     @projects = Project.order(rating: :desc).limit(25)
+
+    @total_users = User.count
+    @banned_users = User.where(is_banned: true).count
+    @burned_amount = Payout.where(user_id: User.where(is_banned: true).select(:id)).where("amount > 0").sum(:amount)
   end
 end
