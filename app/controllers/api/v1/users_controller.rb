63a50bc50b14626a5b301d0663599d56d0a76b78
Sabio <124608899+SabioOfficial@users.noreply.github.com>
2025-07-29 14:22:37 +0200

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-# API endpoint added by Alimadcorp
+# API endpoint added by Alimadcorp & changed slightly by SabioOfficial
 # Provides paginated data for users at /api/v1/users
 # Provides single user at /api/v1/users/:id
+# Provides current user at /api/v1/users/me
 # Only provides whats visible publicly, reference from views/users/show.html.erb
 
 module Api
@@ -99,6 +100,36 @@ module Api
           updated_at: @user.updated_at,
           avatar: @user.avatar,
           custom_css: @user.user_profile&.custom_css
+        }
+      end
+
+      def me
+        user = current_user
+        render json: {
+          id: user.id,
+          slack_id: user.slack_id,
+          display_name: user.display_name,
+          bio: user.user_profile&.bio,
+          projects_count: user.projects.count,
+          devlogs_count: user.devlogs.count,
+          votes_count: user.votes.count,
+          ships_count: user.projects.joins(:ship_events).distinct.count,
+          projects: user.projects.map { |p| { id: p.id, title: p.title, devlogs_count: p.devlogs_count, created_at: p.created_at } },
+          coding_time_seconds: user.has_hackatime? ? user.all_time_coding_seconds : 0,
+          coding_time_seconds_today: user.has_hackatime? ? user.daily_coding_seconds : 0,
+          balance: user.has_badge?(:pocket_watcher) ? (user.has_badge?(:offshore_bank_account) ? "Nice try, but they've covered their tracks a little better than that." : user.balance) : "You need to have a pocket watcher badge to view this.",
+          badges: user.badges.map { |b|
+            icon = b[:icon].include?(".") ? view_context.image_url(b[:icon]) : b[:icon]
+            {
+              name: b[:name],
+              text: b[:flavor_text],
+              icon: icon
+            }
+          },
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+          avatar: user.avatar,
+          custom_css: user.user_profile&.custom_css
         }
       end
     end
