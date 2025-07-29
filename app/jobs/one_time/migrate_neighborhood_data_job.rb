a206d94769add895fceda1ee2c7e6e69b5486424
Max Wofford <max@maxwofford.com>
2025-07-29 15:10:26 -0400

    Rubocop format

--- app/jobs/one_time/migrate_neighborhood_data_job.rb
+++ app/jobs/one_time/migrate_neighborhood_data_job.rb
@@ -16,25 +16,25 @@ class OneTime::MigrateNeighborhoodDataJob < ApplicationJob
       cached_records_from("Post Event - Hour Transfers").map { |r| r&.fields }
     end
     transfers_for_som = cache_with_json("transfers_for_som.json") do
-      transfers.select { |r| r["Which transfer option would you like to go with?"] == "Summer of Making"}
+      transfers.select { |r| r["Which transfer option would you like to go with?"] == "Summer of Making" }
     end
     puts "found #{transfers_for_som.count} hour transfers"
     slack_ids_for_som = transfers_for_som.map { |r| r["What is your Slack ID?"] }.compact.uniq
 
-    filter_for_neighbors = slack_ids_for_som.map{ |slack_id| "{Slack ID (from slackNeighbor)}='#{slack_id}'" }
+    filter_for_neighbors = slack_ids_for_som.map { |slack_id| "{Slack ID (from slackNeighbor)}='#{slack_id}'" }
     neighbors = cache_with_json("neighbors.json") do
       cached_records_from("neighbors", filter: "OR(#{filter_for_neighbors.join(", ")})").map { |r| r&.fields }
     end
     puts "found #{neighbors.count} neighbors"
 
-    filter_for_projects = slack_ids_for_som.map{ |slack_id| "{Slack ID (from slackNeighbor) (from neighbors)}='#{slack_id}'" }
+    filter_for_projects = slack_ids_for_som.map { |slack_id| "{Slack ID (from slackNeighbor) (from neighbors)}='#{slack_id}'" }
     projects = cache_with_json("projects.json") do
       cached_records_from("YSWS Project Submission", filter: "OR(#{filter_for_projects.join(", ")})").map { |r| r&.fields }
     end
     puts "found #{projects.count} projects"
 
     # these are like devlogs
-    filter_for_posts = slack_ids_for_som.map{ |slack_id| "{slackId}='#{slack_id}'" }
+    filter_for_posts = slack_ids_for_som.map { |slack_id| "{slackId}='#{slack_id}'" }
     posts = cache_with_json("posts.json") do
       cached_records_from("Posts", filter: "OR(#{filter_for_posts.join(", ")})").map { |r| r&.fields }
     end
@@ -57,7 +57,7 @@ class OneTime::MigrateNeighborhoodDataJob < ApplicationJob
 
   def table(table_name)
     Norairrecord.table(
-      ENV['NEIGHBORHOOD_AIRTABLE_KEY'],
+      ENV["NEIGHBORHOOD_AIRTABLE_KEY"],
       "appnsN4MzbnfMY0ai",
       table_name
     )
