83b882309194b79b3572dcc6342e4349fe42ec22
Kartikey Chauhan <skartikey314@gmail.com>
2025-08-09 13:16:02 +0530

    tighten queue  (#862)
    * lint
    
    * remove RefillUserVoteQueueJob from set_projects
    
    * queue QUEUE_SIZE instead of REFILL_THRESHOLD
    
    * tighten queue and eliminate the job

--- app/jobs/refill_user_vote_queue_job.rb
+++ app/jobs/refill_user_vote_queue_job.rb
@@ -5,17 +5,6 @@ class RefillUserVoteQueueJob < ApplicationJob
   include UniqueJob
 
   def perform(user_id)
-    user = User.find(user_id)
-    vote_queue = user.user_vote_queue || user.build_user_vote_queue.tap(&:save!)
-
-    vote_queue.with_lock do
-      if vote_queue.needs_refill? || vote_queue.queue_exhausted?
-        generated_count = vote_queue.refill_queue!
-        Rails.logger.info "Refilled vote queue for user #{user_id}: #{generated_count} new matchups"
-      end
-    end
-  rescue => e
-    Rails.logger.error "Failed to refill vote queue for user #{user_id}: #{e.message}"
-    raise e
+    Rails.logger.info "RefillUserVoteQueueJob is deprecated"
   end
 end
