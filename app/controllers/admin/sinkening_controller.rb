8a022cf8ded7e1bb4f97d726262437d50aad1c0a
Kartikey Chauhan <me@cskartikey.dev>
2025-08-11 20:33:51 +0530

    insta update url

--- app/controllers/admin/sinkening_controller.rb
+++ app/controllers/admin/sinkening_controller.rb
@@ -7,8 +7,8 @@ module Admin
     def update
       @setting = SinkeningSetting.current
 
-      if @setting.update(intensity: params[:intensity])
-        flash[:success] = "Sinkening intensity updated to #{@setting.intensity}"
+      if @setting.update(intensity: params[:intensity], slack_story_url: params[:slack_story_url])
+        flash[:success] = "Sinkening settings updated. Intensity: #{@setting.intensity}"
       else
         flash[:error] = "Failed to update intensity: #{@setting.errors.full_messages.join(', ')}"
       end
