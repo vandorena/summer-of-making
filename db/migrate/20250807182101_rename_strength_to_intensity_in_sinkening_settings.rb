29b808cec3141c7c2a83e57199cc1ee475a40542
Sam Liu <sam@samliu.dev>
2025-08-10 22:49:48 -0400

    Fid migration

--- db/migrate/20250807182101_rename_strength_to_intensity_in_sinkening_settings.rb
+++ db/migrate/20250807182101_rename_strength_to_intensity_in_sinkening_settings.rb
@@ -1,5 +1,8 @@
 class RenameStrengthToIntensityInSinkeningSettings < ActiveRecord::Migration[8.0]
   def change
-    rename_column :sinkening_settings, :strength, :intensity
+    # Check if the strength column exists before trying to rename it
+    if column_exists?(:sinkening_settings, :strength)
+      rename_column :sinkening_settings, :strength, :intensity
+    end
   end
 end
