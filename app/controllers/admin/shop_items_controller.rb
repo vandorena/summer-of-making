dcc7a9d177e9cb98f2b94131c0e49f24535554e2
Echo <github@3kh0.net>
2025-08-14 21:51:06 -0400

    mb gang (#913)

--- app/controllers/admin/shop_items_controller.rb
+++ app/controllers/admin/shop_items_controller.rb
@@ -14,7 +14,7 @@ module Admin
     def show
       @shop_orders = @shop_item.shop_orders.includes(:user).order(created_at: :desc).limit(20)
       @total_orders = @shop_item.shop_orders.count
-      @total_spent = @shop_item.shop_orders.sum(:total_cost)
+      @total_spent = @shop_item.shop_orders.sum(:frozen_item_price)
     end
 
     def new
