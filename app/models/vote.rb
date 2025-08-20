da1255c35a3392d1a7909f87f9b23bbc2e47f768
Kartikey Chauhan <me@cskartikey.dev>
2025-08-20 11:21:14 +0530

    bump minimum char count to 100 characters

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -58,7 +58,7 @@ class Vote < ApplicationRecord
 
   has_many :vote_changes, dependent: :destroy
 
-  validates :explanation, presence: true, length: { minimum: 20, maximum: 2000 }
+  validates :explanation, presence: true, length: { minimum: 100, maximum: 2000 }
   validates :status, inclusion: { in: %w[active invalid] }
 
   validates :user_id, uniqueness: {
