5281becb8420248201c199af1b19daf91b932c98
Sam Liu <sam@samliu.dev>
2025-08-25 12:54:57 -0400

    Change gap for seconds

--- app/models/devlog.rb
+++ app/models/devlog.rb
@@ -68,7 +68,13 @@ class Devlog < ApplicationRecord
   end
 
   # ie. Project.first.devlogs.capped_duration_seconds
-  scope :capped_duration_seconds, -> { where.not(duration_seconds: nil).sum("LEAST(duration_seconds, #{10.hours.to_i})") }
+  scope :capped_duration_seconds, lambda {
+    cap_date = Time.new(2025, 7, 19)
+    where.not(duration_seconds: nil)
+      .sum(
+        "CASE WHEN created_at >= '#{cap_date.utc.to_s(:db)}' THEN LEAST(duration_seconds, #{10.hours.to_i}) ELSE duration_seconds END"
+      )
+  }
 
   def recalculate_seconds_coded
     # find the created_at of the devlog directly before this one
