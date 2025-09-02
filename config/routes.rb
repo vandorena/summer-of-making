8085e7e6b5462122e26489f66ec315a8a133a875
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-02 14:59:54 -0400

    add the secret third thing

--- config/routes.rb
+++ config/routes.rb
@@ -349,6 +349,7 @@ Rails.application.routes.draw do
   post "track_view", to: "view_tracking#create"
 
   get "/gork", to: "static_pages#gork"
+  get "/s", to: "static_pages#s", as: :stt
 
   namespace :admin, constraint: AdminConstraint do
     mount MissionControl::Jobs::Engine, at: "jobs"
