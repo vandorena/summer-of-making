cc99204f04cb0c0ad3e56669d1138d5825fbdedc
Kartikey Chauhan <me@cskartikey.dev>
2025-09-03 19:21:42 +0530

    feat: improve voting comms + handle rollover votes

--- app/models/project.rb
+++ app/models/project.rb
@@ -324,7 +324,7 @@ class Project < ApplicationRecord
         message: "You must have at least one devlog #{ship_events.count > 0 ? "since the last ship" : ""}"
       },
       voting_quota: {
-        met: user.votes_since_last_ship_count >= 20,
+        met: user.can_ship_by_votes?,
         message: "You must vote #{user.remaining_votes_to_ship} more times to ship."
       },
       repo_link: {
