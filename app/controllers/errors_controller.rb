32f67c4c46a8c64702bed0938e7b84ca68648de8
Sam Liu <sam@samliu.dev>
2025-09-06 00:48:58 -0400

    Prevent looping of 404 or 500 errors for images

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,15 @@
 class ErrorsController < ApplicationController
- def not_found
-   render status: 404
- end
+  def not_found
+    respond_to do |format|
+      format.html { render "errors/not_found", status: :not_found }
+      format.any  { head :not_found }
+    end
+  end
 
- def internal_server
-   render status: 500
- end
+  def internal_server
+    respond_to do |format|
+      format.html { render "errors/internal_server", status: :internal_server_error }
+      format.any  { head :internal_server_error }
+    end
+  end
 end
