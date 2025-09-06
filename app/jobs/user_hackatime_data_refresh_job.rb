555cb21f2ef548d0a84eb512f42448397251b543
Kartikey Chauhan <me@cskartikey.dev>
2025-09-07 01:39:40 +0530

    working concurrency controls :skull:

--- app/jobs/user_hackatime_data_refresh_job.rb
+++ app/jobs/user_hackatime_data_refresh_job.rb
@@ -2,7 +2,7 @@
 
 # This refreshes every user's Hackatime Project data.
 class UserHackatimeDataRefreshJob < ApplicationJob
-  include UniqueJob
+  limits_concurrency to: 1, key: -> { "UserHackatimeDataRefreshJob:singleton" }, duration: 2.hours, on_conflict: :block
 
   queue_as :literally_whenever
 
