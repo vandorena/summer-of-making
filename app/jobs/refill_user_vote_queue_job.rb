df25688df7a9d5053a2c9909358c03c24b20c7e4
Kartikey Chauhan <me@cskartikey.dev>
2025-09-07 10:18:52 +0530

    ensure concurrency

--- app/jobs/refill_user_vote_queue_job.rb
+++ app/jobs/refill_user_vote_queue_job.rb
@@ -2,7 +2,10 @@
 
 class RefillUserVoteQueueJob < ApplicationJob
   queue_as :default
-  include UniqueJob
+  limits_concurrency to: 1,
+                      key: ->(user_id) { "RefillUserVoteQueueJob:user:#{user_id}" },
+                      duration: 15.minutes,
+                      on_conflict: :discard
 
   def perform(user_id)
     user = User.find_by(id: user_id)
