794262bfde1c4f1483e1c7d23c6d894daa219b11
Kartikey Chauhan <me@cskartikey.dev>
2025-09-10 04:35:48 +0530

    a bit safer

--- app/jobs/active_storage_move_job.rb
+++ app/jobs/active_storage_move_job.rb
@@ -1,18 +1,55 @@
 class ActiveStorageMoveJob < ApplicationJob
     queue_as :default
 
-    def perform(source = "cloudflare", destination = "cloudflare_new")
-      blobs = ActiveStorage::Blob.where(service_name: source)
+    # a bit safer + it'll DM @cskartikey!
+    def perform(source = "cloudflare", destination = "cloudflare_new", notify_user_id = 4)
+        destination_service = ActiveStorage::Blob.services.fetch(destination.to_sym)
 
-      blobs.find_each do |blob|
-        blob.open do |file|
-          new_key = ActiveStorage::Blob.generate_unique_secure_token
-          service = ActiveStorage::Blob.services.fetch(destination.to_sym)
+        notify_slack_id = User.find_by(id: notify_user_id)&.slack_id
 
-          service.upload(new_key, file, checksum: blob.checksum)
+        scope = ActiveStorage::Blob.where(service_name: source)
+        total_to_process = scope.count
+        migrated_count = 0
 
-          blob.update!(key: new_key, service_name: destination)
+        scope.in_batches(of: 1000) do |relation|
+            relation.each do |blob|
+                begin
+                    unless destination_service.exist?(blob.key)
+                        blob.open do |file|
+                            destination_service.upload(
+                                blob.key,
+                                file,
+                                checksum: blob.checksum,
+                                content_type: blob.content_type
+                            )
+                        end
+                    end
+
+                    unless destination_service.exist?(blob.key)
+                        raise "Destination missing object after upload: #{blob.key}"
+                    end
+
+                    blob.update_columns(service_name: destination)
+                    migrated_count += 1
+
+                    if notify_slack_id.present? && (migrated_count % 1000 == 0)
+                        SendSlackDmJob.perform_later(
+                            notify_slack_id,
+                            "Active Storage migration: migrated #{migrated_count}/#{total_to_process} blobs to #{destination}."
+                        )
+                    end
+                rescue => e
+                    Honeybadger.notify(e, context: { blob_id: blob.id, key: blob.key, source: source, destination: destination })
+                    next
+                end
+            end
         end
-      end
+
+        if notify_slack_id.present?
+            SendSlackDmJob.perform_later(
+                notify_slack_id,
+                "Active Storage migration complete: migrated #{migrated_count}/#{total_to_process} blobs to #{destination}."
+            )
+        end
     end
 end
