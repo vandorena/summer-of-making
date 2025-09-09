63eb2e9c11e466b29a70ce6d4f047b0dc401ea02
Max Wofford <max@maxwofford.com>
2025-09-09 14:59:29 -0400

    Add if_not_exists on disabled_ddl for project_languages index

--- db/migrate/20250909183009_add_index_to_project_languages_on_language_stats.rb
+++ db/migrate/20250909183009_add_index_to_project_languages_on_language_stats.rb
@@ -2,6 +2,6 @@ class AddIndexToProjectLanguagesOnLanguageStats < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_index :project_languages, :language_stats, using: :gin, algorithm: :concurrently
+    add_index :project_languages, :language_stats, using: :gin, algorithm: :concurrently, if_not_exists: true
   end
 end
