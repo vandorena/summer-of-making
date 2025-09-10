e8e6526cdd721c6e51fc7653b8696c4d5cdc5fcb
Max Wofford <max@maxwofford.com>
2025-09-10 19:10:20 -0400

    Comment other references to ahoy captain

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -18,7 +18,7 @@
 #  index_ahoy_events_on_visit_id       (visit_id)
 #
 class Ahoy::Event < ApplicationRecord
-  include AhoyCaptain::Ahoy::EventMethods
+  # include AhoyCaptain::Ahoy::EventMethods
   include Ahoy::QueryMethods
 
   self.table_name = "ahoy_events"
