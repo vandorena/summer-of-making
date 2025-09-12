ada7d5b67ecac572c4f03c484c08498c57bd62ea
Max Wofford <max@maxwofford.com>
2025-09-12 15:35:25 -0400

    Reimplement hb rate limiter to use increment instead of ts stack
    Also turns out honeybadger has a fingerprint itself so I don't need to construct keys based on classes

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,3 +1,9 @@
+require "honeybadger_rate_limiter"
+
 Honeybadger.configure do |config|
   config.api_key = Rails.application.credentials.honeybadger_auth
+
+  config.before_notify do |notice|
+    throw :skip unless HoneybadgerRateLimiter.allow?(notice)
+  end
 end
