ff99793896e7820ea0b51a96ace09dbdbf8f3e77
Max Wofford <max@maxwofford.com>
2025-09-12 19:25:52 -0400

    Disable honeybadger limiter for now

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,9 +1,9 @@
-require "honeybadger_rate_limiter"
+# require "honeybadger_rate_limiter"
 
 Honeybadger.configure do |config|
   config.api_key = Rails.application.credentials.honeybadger_auth
 
-  config.before_notify do |notice|
-    throw :skip unless HoneybadgerRateLimiter.allow?(notice)
-  end
+  # config.before_notify do |notice|
+  #   throw :skip unless HoneybadgerRateLimiter.allow?(notice)
+  # end
 end
