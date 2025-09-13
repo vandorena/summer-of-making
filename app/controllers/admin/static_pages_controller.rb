128277e3137a5c2e00d43b936b034a845cdb346f
Max Wofford <max@maxwofford.com>
2025-09-13 14:54:00 -0400

    Opinionated pass on admin dashboard

--- app/controllers/admin/static_pages_controller.rb
+++ app/controllers/admin/static_pages_controller.rb
@@ -6,11 +6,8 @@ module Admin
     def index
       render html: "<h1>bruh</h1>".html_safe unless current_user&.is_admin? || current_user&.fraud_team_member?
 
-      # Generate data points for Payout.calculate_multiplier from 0 to 1 in steps of 0.01
-      # @multiplier_data = (0..100).map do |i|
-      #   x = i / 100.0
-      #   [ x, Payout.calculate_multiplier(x) ]
-      # end
+      @admin_signed_in = current_user.is_admin?
+      @fraud_team_signed_in = current_user.fraud_team_member?
     end
 
     private
