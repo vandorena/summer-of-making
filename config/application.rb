2d87fcff5f2842ac3be2bf692c80ce03348297c8
Kartikey Chauhan <skartikey314@gmail.com>
2025-09-14 05:51:06 +0530

    switch to EST (#1116)

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Journey
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Eastern Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("app/models/shop_item")
     # config.autoload_paths << Rails.root.join("app/models/shop_item")
     config.autoload_paths << Rails.root.join("app/presenters")
